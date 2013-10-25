50d1128bfa032fe50161b00924a0cbd530a8ce24
Julia Glushko <jan-glu@yandex.ru>
2013-10-25 17:26:40 +0400

    Add template files

--- practice_2/mathConst.py
+++
@@ -1,9 +0,0 @@
-#encoding utf-8
-import math
-constDict={"pi":math.pi,"e":math.e}
-s=raw_input("enter math constant:precision\n ")
-l=s.split(":")
-for k in constDict:
-    if l[0]==k:
-        print(round(constDict[k],int(l[1])))
-
